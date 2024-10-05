04924634583196893746e6e9361cc3db780a1768
Gvinfinity <g.soares@ifpeopensource.com.br>
2024-10-05 18:32:23 -0300

    feat: Update eras

--- src/pages/Simulation.tsx
+++ src/pages/Simulation.tsx
@@ -24,27 +24,37 @@ const Simulation: React.FC = () => {
     const [currentEvent, setCurrentEvent] = React.useState<string>("");
 
     const [backgroundUrl, setBackgroundUrl] = React.useState<string>("");
+    const [imageUrl, setImageUrl] = React.useState<string>("");
 
     const globeRef = React.useRef<GlobeMethods | undefined>();
     const rotate = async (globeRef: React.MutableRefObject<GlobeMethods | undefined>) => {
+        setBackgroundUrl("");
         if (globeRef.current) {
-            console.log("Globe ref", globeRef.current);
             const controls = globeRef.current.controls();
+            controls.enableRotate = false;
             controls.autoRotate = true;
             globeRef.current.pointOfView({ lat: 0, lng: 0 });
             for (let i = 500; i > 0; i -= 1) {
                 controls.autoRotateSpeed = i;
                 await new Promise((resolve) => setTimeout(resolve, 0.5));
             }
+            controls.enableRotate = true;
             controls.autoRotate = false;
         }
+        setTimeout(() => {
+            setBackgroundUrl("./background.jpg");
+        }, 300);
     }
 
     React.useEffect(() => {
-        rotate(globeRef).then(() => {
-            setBackgroundUrl("./background.jpg");
-        });
+        rotate(globeRef);
     }, []);
+
+    React.useEffect(() => {
+        if (currentEvent === "First Extinction") setImageUrl("./first_extinction.png");
+        else if (currentEvent === "Second Extinction") setImageUrl("./second_extinction.png");
+        rotate(globeRef);
+    }, [currentEvent]);
 
     const Timeline: React.FC = () => {
         return (
@@ -54,13 +64,14 @@ const Simulation: React.FC = () => {
             </div>
         );
     }
+    
 
     return (
         <>
             <div id="simulation">
                 {/* <h1 className="absolute z-10 transform translate-x-1/2 bottom-20 text-4xl text-white text-center">{currentEvent}</h1> */}
                 <Timeline />
-                <EarthSimulation imageUrl={"./first_extinction.png"} globeRef={globeRef} backgroundUrl={backgroundUrl}/>
+                <EarthSimulation imageUrl={imageUrl ? imageUrl:"./nowadays.png"} globeRef={globeRef} backgroundUrl={backgroundUrl}/>
             </div>
         </>
     );
