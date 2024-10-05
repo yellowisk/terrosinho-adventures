eae01177b58032135b5a854c4af2e59a4b28cda9
Gvinfinity <g.soares@ifpeopensource.com.br>
2024-10-05 12:13:22 -0300

    Add basic simulation

--- src/pages/Simulation.tsx
+++ src/pages/Simulation.tsx
@@ -1,14 +1,31 @@
 import React from "react";
 import Globe from "react-globe.gl";
 
-const EarthSimulation: React.FC<undefined> = () => {
+interface EarthSimulationProps {
+  imageUrl: string;
+}
+
+const EarthSimulation: React.FC<EarthSimulationProps> = ({ imageUrl }) => {
+    return (
+        <Globe 
+            globeImageUrl={imageUrl}
+            backgroundImageUrl={"./background.jpg"}
+            animateIn={true}
+        />
+    );
+}
+
+const Simulation: React.FC = () => {
     return (
         <>
-            <Globe 
-                backgroundImageUrl={"https://raw.githubusercontent.com/chrisrzhou/react-globe/main/textures/background.png"}
-            />
+            <div id="simulation">
+                <div className="absolute z-10 rounded bg-slate-500 ms-5 mt-5 opacity-50 w-1/3 h-1/4">
+                    <h1 className="text-black">AAAAA</h1>
+                </div>
+                <EarthSimulation imageUrl={"./first_extinction.png"}/>
+            </div>
         </>
     );
 }
 
-export default EarthSimulation;
+export default Simulation;
