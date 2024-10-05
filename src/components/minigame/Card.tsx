448397e55b286d8ee39d670d2fcfccae753d3109
yellowisk <oheitoralmeida2013@gmail.com>
2024-10-05 17:38:11 -0300

    feat: add card

--- src/components/minigame/Card.tsx
+++ src/components/minigame/Card.tsx
@@ -1,15 +1,19 @@
 import { LucideIcon } from 'lucide-react'; // Import the LucideIcon type
 
 interface CardProps {
-  text: string; // Text to display on the card
-  icon: LucideIcon; // Icon prop of type LucideIcon
+  text: string;
+  icon: LucideIcon;
+  onClick?: () => void;
 }
 
-const Card: React.FC<CardProps> = ({ text, icon: Icon }) => {
+const Card: React.FC<CardProps> = ({ text, icon: Icon, onClick }) => {
   return (
-    <div className="flex flex-col items-center justify-center h-20 w-36 bg-white rounded-lg shadow-lg cursor-pointer">
-        <Icon className="w-8 h-8 text-blue-500" />
-        <div className="mt-2 text-center text-black">{text}</div>
+    <div 
+      className="flex flex-col items-center justify-center h-20 w-36 bg-white rounded-lg shadow-lg cursor-pointer" 
+      onClick={onClick}
+    >
+      <Icon className="w-8 h-8 text-blue-500" />
+      <div className="mt-2 text-center text-black">{text}</div>
     </div>
   );
 }
