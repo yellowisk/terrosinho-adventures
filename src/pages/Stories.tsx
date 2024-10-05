b1e1594814a8cf3e172732db0635120524ba1d37
Milena Shishito <milena.shishito@ifood.com.br>
2024-10-05 17:42:44 -0300

    feat: Update stories

--- src/pages/Stories.tsx
+++ src/pages/Stories.tsx
@@ -5,6 +5,10 @@ import { story2 } from "../stories/story-2";
 
 import styled from "styled-components";
 import BackButton from "../components/BackButton";
+import { story3 } from "../stories/story-3";
+import { story4 } from "../stories/story-4";
+import { story5 } from "../stories/story-5";
+import { story6 } from "../stories/story-6";
 
 const StoriesContainer = styled.div`
   display: flex;
@@ -12,12 +16,12 @@ const StoriesContainer = styled.div`
 `;
 
 const Stories: React.FC = () => {
-  const stories = [story1, story2];
+  const stories = [story1, story2, story3, story4, story5, story6];
   return (
     <div className="p-16">
       <BackButton />
       <h1 className="text-5xl font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-purple-400 via-pink-500 to-red-500 drop-shadow-lg mb-8">
-       Stories
+        Stories
       </h1>
       <StoriesContainer>
         {stories.map((story) => (
